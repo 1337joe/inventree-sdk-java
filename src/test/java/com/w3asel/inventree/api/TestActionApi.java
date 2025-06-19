65be54e9480e15d254a60af0a5ad9f05a7e24e24
Joe Rogers <1337joe@gmail.com>
2025-06-18 23:12:30 -0400

    Add missing test classes

---
+++ src/test/java/com/w3asel/inventree/api/TestActionApi.java
@@ -0,0 +1,21 @@
+package com.w3asel.inventree.api;
+
+import com.w3asel.inventree.invoker.ApiException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class TestActionApi extends TestApi {
+    private ActionApi api;
+
+    @BeforeEach
+    public void setup() {
+        api = new ActionApi(apiClient);
+    }
+
+    @Disabled
+    @Test
+    public void todo() throws ApiException {
+        api.actionCreate(null);
+    }
+}
