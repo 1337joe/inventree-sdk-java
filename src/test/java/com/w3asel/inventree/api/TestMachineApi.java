c1ed9b16743b4fea9bbaa9583b9a2251b54d9c70
Joe Rogers <1337joe@gmail.com>
2025-10-21 14:43:36 -0400

    Add machine api test

--- src/test/java/com/w3asel/inventree/api/TestMachineApi.java
+++ src/test/java/com/w3asel/inventree/api/TestMachineApi.java
@@ -1,9 +1,13 @@
 package com.w3asel.inventree.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.w3asel.inventree.invoker.ApiException;
+import com.w3asel.inventree.model.MachineType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 public class TestMachineApi extends TestApi {
     private MachineApi api;
@@ -28,7 +32,7 @@ public class TestMachineApi extends TestApi {
         api.machineSettingsRetrieve(null, null, null);
         api.machineSettingsUpdate(null, null, null, null);
         api.machineStatusRetrieve();
-        api.machineTypesList();
+        // api.machineTypesList();
         api.machineUpdate(null, null);
     }
 
@@ -38,8 +42,19 @@ public class TestMachineApi extends TestApi {
         int limit = 1000;
         api.machineDriversList();
         api.machineList(limit, null, null, null, null, null, null);
-        api.machineTypesList();
-        // api.machineSettingsList(null)
+        // api.machineSettingsList(null);
         api.machineStatusRetrieve();
+    }
+
+    @Test
+    void machineTypesList() throws ApiException {
+        List<MachineType> actual = api.machineTypesList();
+
+        assertEquals(1, actual.size(), "Incorrect machine type count");
+
+        MachineType actualLabelPrinter = actual.get(0);
+        assertEquals("label-printer", actualLabelPrinter.getSlug(), "Incorrect slug");
+        assertEquals("Label Printer", actualLabelPrinter.getName(), "Incorrect name");
+        assertTrue(actualLabelPrinter.getIsBuiltin(), "Incorrect built-in");
     }
 }
