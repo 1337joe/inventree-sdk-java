e4303d9a447a876d680bb2488c753d6418de3d47
Joe Rogers <1337joe@gmail.com>
2026-08-03 23:19:36 -0600

    Update to schema 520 (#299)
    Adds new "disassemble" API endpoint for stock items
    Allows a stock item to be broken down into component parts, based on its Bill of Materials

--- src/test/java/com/w3asel/inventree/api/TestStockApi.java
+++ src/test/java/com/w3asel/inventree/api/TestStockApi.java
@@ -63,6 +63,7 @@ public class TestStockApi extends TestApi {
         // api.stockCreate(null);
         // api.stockBulkDestroy(null);
         api.stockDestroy(null);
+        api.stockDisassembleCreate(null, null);
         api.stockInstallCreate(null, null);
         // api.stockList(null, null, null, null, null, null, null, null, null, null, null, null,
         // null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
